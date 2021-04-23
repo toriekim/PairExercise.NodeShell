de8c301586b78875005c59f6f4fb90d702e4acdc
Helena Bliss <helena_bliss@LAPTOP-R25MGT9J.localdomain>
2021-04-23 13:45:08 -0500

    added space

--- bash.js
+++ bash.js
@@ -2,6 +2,7 @@
 process.stdout.write('prompt > ');
 
 //The stdin 'data' event fires after a user types in a line
+
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
     if (cmd ==='pwd'){
