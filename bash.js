0aea4e7fb1282fc2c49724213b09a109bef2a4d3
Victoria Kim <vkim20@gmail.com>
2021-04-23 16:07:00 -0400

    Update bash.js

--- bash.js
+++ bash.js
@@ -1,1 +1,20 @@
-//This is our bash file
+// Output a prompt
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a line
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd ==='pwd'){
+        const pwd = require('./pwd')
+        pwd()
+    }
+    else if (cmd === 'ls') {
+        const ls = require('./ls')
+        ls()
+    }
+    else{
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+});
