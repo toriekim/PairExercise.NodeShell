7f7c19a3725fc1d23b41c7ed8dc46692131ee649
Helena Bliss <helena_bliss@LAPTOP-R25MGT9J.localdomain>
2021-04-23 13:32:31 -0500

    added prompt and pwd

--- bash.js
+++ bash.js
@@ -1,1 +1,17 @@
-//This is our bash file
+// Output a prompt
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd ==='pwd'){
+        let workingDir = process.cwd();
+        process.stdout.write(workingDir);
+        process.stdout.write('\nprompt > ');
+    }
+    else{
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
+    
+});
